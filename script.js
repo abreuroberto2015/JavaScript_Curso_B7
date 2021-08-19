a0b59c8db0b41dab194bf3ea57e1443042c04612
Roberto Abreu <abreuroberto2015@gmail.com>
2021-08-19 19:34:41 -0300

    19/08

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-var nome = "Beto";
-var idade = 90;
-var salvo = true;
-var cidade;
+function alterar(titulo){
+    document.getElementById("titulo").innerHTML = titulo;
+    document.getElementById("campo").value = titulo;
+}
+
+function somar(x, y){
+    let total = x + y;
+
+    return total;
+}
+
+var resultado = somar(10, 15);
+
+console.log(resultado);
