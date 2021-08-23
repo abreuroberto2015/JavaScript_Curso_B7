46c3f467d5a0e79f92d19f98d76c4371b6a600ac
Roberto Abreu <abreuroberto2015@gmail.com>
2021-08-23 18:32:17 -0300

    23/08

---
+++ Modulo03/array/script.js
@@ -0,0 +1,19 @@
+/*let carros =[
+    'Palio',
+    'Uno',
+    'Corolla',
+    'Ferrari',
+    function(){
+        console.log('Testando 1,2,3....')
+    }
+];
+
+carros[4]();
+*/
+
+let ingredientes = [
+    ['uva', 'pera', 'maça'],
+    ['arroz', 'macarrão']
+];
+
+console.log(ingredientes[1])
